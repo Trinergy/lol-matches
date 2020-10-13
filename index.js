cebb9aa07766504380b5923a0b5fc736683bc450
Kenny Wu <contact@kennywupaints.com>
2020-10-13 13:29:31 -0700

    Add requests to search for recent 5 matches

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ var RiotRequest = require('riot-lol-api');
  
 var riotRequest = new RiotRequest(process.env.RIOT_API_KEY);
 
+// Get encrypted Account ID by summoner name
 riotRequest.request('na1', 'summoners', '/lol/summoner/v4/summoners/by-name/Doublelift', function(err, data) {
-  console.log(data)
+  const accountId = data.accountId
+  const matchCount = 5
+
+  // Get recent 5 matches of summoner
+  riotRequest.request('na1', 'match', `/lol/match/v4/matchlists/by-account/${accountId}?endIndex=${matchCount}`, function(err, data) {
+    const matches = data.matches;
+    const gameIds = [];
+    
+    // Parse for GameIds
+    matches.forEach(function(match){
+      gameIds.push(match.gameId)
+    });
+
+    // Get individual match data
+    gameIds.forEach(function(gameId) {
+      riotRequest.request('na1', 'match', `/lol/match/v4/matches/${gameId}`, function(err, data) {
+        console.log(data)
+      })
+    });
+
+  })
 });
